74e630d729a7863e8aee6523669407aa69d2319c
Wanaldino Antimonio <carlosmm31093@gmail.com>
2020-08-31 15:31:49 +0200

    Fixed selectStation function logic

---
+++ Prueba tecnica RynairTests/FormInteractorTest.swift
@@ -0,0 +1,9 @@
+//
+//  FormInteractorTest.swift
+//  Prueba tecnica RynairTests
+//
+//  Created by Wanaldino Antimonio on 31/08/2020.
+//  Copyright © 2020 Carlos Martinez Medina. All rights reserved.
+//
+
+import Foundation
